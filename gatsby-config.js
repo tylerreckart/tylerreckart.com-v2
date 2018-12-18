381d606ee9a04b7c8595e804ad4c3a900e034e5c
Tyler Reckart <tyler.reckart@gmail.com>
2018-12-17 20:18:52 -0500

    Configure gatsby google analytics plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,21 +20,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: "YOUR_GOOGLE_ANALYTICS_TRACKING_ID",
-        // Puts tracking script in the head instead of the body
+        trackingId: "UA-70418516-1",
         head: false,
-        // Setting this parameter is optional
-        anonymize: true,
-        // Setting this parameter is also optional
-        respectDNT: true,
-        // Avoids sending pageview hits from custom paths
-        exclude: ["/preview/**", "/do-not-track/me/too/"],
-        // Enables Google Optimize using your container Id
-        optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
-        // Any additional create only fields (optional)
-        sampleRate: 5,
-        siteSpeedSampleRate: 10,
-        cookieDomain: "example.com",
       },
     },
     {
