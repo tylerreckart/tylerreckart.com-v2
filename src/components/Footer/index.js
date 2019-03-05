65f94e347480904b6e24018131775d2f6d6e5cc0
Tyler Reckart <tyler.reckart@gmail.com>
2019-03-04 20:14:21 -0500

    Dynamically grab current year for footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
+const currentYear = new Date().getFullYear();
+
 export default () => (
   <div id="footer">
-    <p>&copy; 2013-2018 Tyler Reckart. All Rights Reserved.</p>
+    <p>&copy; 2013-{currentYear} Tyler Reckart. All Rights Reserved.</p>
   </div>
 );
