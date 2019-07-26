534113c05de3b89e98e1d741574d58536683ad2d
Tyler Reckart <tyler.reckart@gmail.com>
2019-07-26 10:57:11 -0400

    Improve footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,11 +1,35 @@
-import React from 'react';
-import Castle from '../Svg/castle-unified';
+import React from "react";
+import Link from "gatsby-link";
+import Castle from "../Svg/castle-unified";
 
 const currentYear = new Date().getFullYear();
 
 export default () => (
   <div id="footer">
-    <Castle className="castle" fill="#20262C" />
-    <span className="copy">&copy; 2013-{currentYear} Tyler Reckart. All Rights Reserved.</span>
+    {/* <Castle className="castle" fill="#6C7D8F" /> */}
+    <ul className="footer-nav">
+      <li>
+        <Link to="/">Blog</Link>
+      </li>{'|'}
+      <li>
+        <Link to="/work">Work</Link>
+      </li>{'|'}
+      <li>
+        <Link to="/about">About</Link>
+      </li>{'|'}
+      <li>
+        <a href="https://github.com/tylerreckart" target="_blank">
+          Github
+        </a>
+      </li>{'|'}
+      <li>
+        <a href="https://twitter.com/tylerreckart" target="_blank">
+          Twitter
+        </a>
+      </li>
+    </ul>
+    <span className="copy">
+      &copy; 2013-{currentYear} Tyler Reckart. All Rights Reserved.
+    </span>
   </div>
 );
