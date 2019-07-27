d75a5431c13ce38be12b3fc3abc8e3cf13162017
Tyler Reckart <tyler.reckart@gmail.com>
2019-07-26 22:53:19 -0400

    Don't index dashboard templates

--- src/templates/admin.js
+++ src/templates/admin.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import Header from '../pages/dashboard/components/Header';
-import TemplateWrapper from './layout';
+import React from "react";
+import Helmet from "react-helmet";
+import Header from "../pages/dashboard/components/Header";
+import TemplateWrapper from "./layout";
 
 const AdminViewTemplate = ({ children }) => (
   <TemplateWrapper>
+    <Helmet title="nordkirchen" meta={[{ name: "robots", content: "noindex" }]} />
     <Header />
     <div id="template">
-      <div id="content-wrapper">
-        {children}
-      </div>
+      <div id="content-wrapper">{children}</div>
     </div>
   </TemplateWrapper>
 );
